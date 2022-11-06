f0bc73192172e7843119e63cd503dfe5a32844a5
t-gv <t.girouxveilleux@cegepmontpetit.ca>
2022-11-05 21:32:45 -0400

    fix dark side

--- docs/javascript/images_dark.js
+++ docs/javascript/images_dark.js
@@ -1,29 +1,47 @@
-var isDark = document.querySelector("body[data-md-color-scheme=slate]");
-if (isDark) {
-  var darkables = document.querySelectorAll('img[src$="darkable"');
-  fromLightToDark(darkables);
-} else {
-  var darkables = document.querySelectorAll('img[src$="darkable"');
-  fromDarkToLight(darkables);
+
+var verifyDark = function(evt){
+
+  setTimeout(function () {
+    if (evt && !evt.target.checked) return;
+    var isDark = document.querySelector("body[data-md-color-scheme=slate]");
+    var darkables = document.querySelectorAll('img[src$="darkable"');
+    if (isDark) {
+      fromLightToDark(darkables);
+    } else {
+      fromDarkToLight(darkables);
+    }
+  });
+
 }
+
+
+verifyDark();
 
 var items = document.querySelectorAll("[data-md-color-media]");
 
+
+
 for (let index = 0; index < items.length; index++) {
   const element = items[index];
-  element.onchange = (evt) => {
-    setTimeout(function () {
-      if (!evt.target.checked) return;
-      var isDark = document.querySelector("body[data-md-color-scheme=slate]");
-      var darkables = document.querySelectorAll('img[src$="darkable"');
-      if (isDark) {
-        fromLightToDark(darkables);
-      } else {
-        fromDarkToLight(darkables);
-      }
-    });
-  };
+  element.onchange = verifyDark();
 }
+
+const observer = new PerformanceObserver((list) => {
+  for (const entry of list.getEntries()) {
+    console.log(entry.name);
+    console.log(entry.initiatorType);
+    if (entry.initiatorType === "fetch") {
+      if(entry.name.endsWith("search_index.json")) continue;
+    
+      verifyDark();
+    }
+  }
+});
+
+observer.observe({
+  entryTypes: ["resource"]
+});
+
 
 function fromLightToDark(images) {
   images.forEach((image) => {
